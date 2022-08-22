c57d9b2c77fbbe11019c7d40b2072369336061a9
casulemarc <casulemarc2014@gmail.com>
2022-08-21 20:18:31 -0400

    ⚡️ Console, Comments, Variables, Methods, Operators

---
+++ variables/script.js
@@ -0,0 +1,54 @@
+// How to Print data to console
+console.log("hi there");
+
+// Variables
+// let and const
+let name = "mKasule Coder";
+console.log("My name is " + name);
+
+let age = 2002;
+let today = 2022;
+let response = 2002 < 1;
+let response1 = 2002 > 1;
+console.log("My age is " + (today - age) + " years old");
+console.log(response);
+console.log(response1);
+
+const name2 = "mKasule Coder";
+today = "Coder";
+console.log("Year is " + today);
+
+// Arithmetic Operations
+let x = 4;
+let y = 5;
+let add = x + y;
+let subtract = x - y;
+let multiply = x * y;
+let div = x / y;
+let remainder = x % y;
+
+let greater = x && y > 1;
+let lesser = (add || y) > 1;
+console.log(greater);
+console.log("Lesser: " + lesser);
+
+// Methods
+// Round OFF
+let roundOff = Math.round(div);
+console.log(roundOff);
+
+// Random Number
+let randomNumber = Math.random(9);
+console.log(randomNumber);
+
+// Floor
+let floorNUmber = Math.floor(randomNumber);
+console.log(floorNUmber);
+
+//Length of strings
+const word = "Javascript is Fun";
+console.log(word.length);
+
+// Characters
+let letter = word.charAt(2);
+console.log(letter);
